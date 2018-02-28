d74f5e43a4190269d64fce0679010ac134f08c24
weimargc <weimargc@gmail.com>
2018-02-28 18:33:17 -0500

    Configuración de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDQnvhz4DCB5Y9mJzAm9HEjfk288pzeCuk",
+    authDomain: "loginapp-f9fac.firebaseapp.com",
+    databaseURL: "https://loginapp-f9fac.firebaseio.com",
+    projectId: "loginapp-f9fac",
+    storageBucket: "loginapp-f9fac.appspot.com",
+    messagingSenderId: "729205733392"
+}
 };
